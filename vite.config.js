0f61c349026214f96e90d964aeaf069172c8729b
Heroeen90 <124597053+Heroeen90@users.noreply.github.com>
2026-06-11 20:11:17 +0300

    Set base path for relative routing in vite config
    Added a base path configuration for relative paths in APK.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // <-- تمت إضافة هذا السطر هنا ليجعل المسارات نسبية لتعمل داخل الـ APK
   plugins: [react()],
   optimizeDeps: {
     // Monaco handles its own workers via CDN loader
